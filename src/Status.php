bc1e09491b2ccbd17f870cd0039ff1eb6ea28f40
Nicholas <47761650+user-error-pixel@users.noreply.github.com>
2026-07-03 21:50:13 -0700

    Remove unnecessary whitespaces (blank lines) in Status.php

--- src/Status.php
+++ src/Status.php
@@ -8,10 +8,10 @@ namespace PixelError\Friendships;
 class Status
 {
     public const PENDING = 0;
-    
+
     public const ACCEPTED = 1;
-    
+
     public const DENIED = 2;
-    
+
     public const BLOCKED = 3;
 }
